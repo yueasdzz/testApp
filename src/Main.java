2e93f57d31533b779920329884fa024c6f911f6d
MokYuenYua <mokyuenyua@gmail.com>
2024-06-15 00:53:24 +0800

    apptest_test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world! i can push to github now!");
-
+        System.out.println("Hello world!");
     }
 }
